de9b90ebb2b7e51488fddb188da5ae808548b550
zer0fire <1076671121@qq.com>
2018-05-06 11:31:24 +0800

    完善main.py及其函数，用调用命令行的方式让MQTT传递信息

--- MyAgent.py
+++ MyAgent.py
@@ -13,16 +13,16 @@ class Ui_Form(object):
         Form.setObjectName("Form")
         Form.resize(812, 431)
         self.OpenFansButton = QtWidgets.QPushButton(Form)
-        self.OpenFansButton.setGeometry(QtCore.QRect(350, 290, 171, 41))
+        self.OpenFansButton.setGeometry(QtCore.QRect(350, 340, 171, 41))
         self.OpenFansButton.setObjectName("OpenFansButton")
         self.CloseFansButton = QtWidgets.QPushButton(Form)
-        self.CloseFansButton.setGeometry(QtCore.QRect(540, 290, 171, 41))
+        self.CloseFansButton.setGeometry(QtCore.QRect(540, 340, 171, 41))
         self.CloseFansButton.setObjectName("CloseFansButton")
         self.Year = QtWidgets.QTextBrowser(Form)
-        self.Year.setGeometry(QtCore.QRect(350, 90, 171, 121))
+        self.Year.setGeometry(QtCore.QRect(310, 40, 171, 121))
         self.Year.setObjectName("Year")
         self.Time = QtWidgets.QTextBrowser(Form)
-        self.Time.setGeometry(QtCore.QRect(540, 90, 171, 121))
+        self.Time.setGeometry(QtCore.QRect(520, 40, 171, 121))
         self.Time.setObjectName("Time")
         self.Page1Button = QtWidgets.QPushButton(Form)
         self.Page1Button.setGeometry(QtCore.QRect(0, 40, 121, 31))
@@ -33,6 +33,9 @@ class Ui_Form(object):
         self.Page3Button = QtWidgets.QPushButton(Form)
         self.Page3Button.setGeometry(QtCore.QRect(0, 100, 121, 31))
         self.Page3Button.setObjectName("Page3Button")
+        self.Status = QtWidgets.QTextBrowser(Form)
+        self.Status.setGeometry(QtCore.QRect(310, 170, 171, 61))
+        self.Status.setObjectName("Status")
 
         self.retranslateUi(Form)
         self.OpenFansButton.clicked.connect(Form.OpenFans)
@@ -50,4 +53,10 @@ class Ui_Form(object):
         self.Page1Button.setText(_translate("Form", "仪表"))
         self.Page2Button.setText(_translate("Form", "按键"))
         self.Page3Button.setText(_translate("Form", "附加项"))
+        self.Status.setHtml(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'SimSun\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt;\">状态：</span></p></body></html>"))
 
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QMessageBox
 import sys
+import os
+#import paho.mqtt
 
 from MyAgent import Ui_Form
 
@@ -16,10 +18,12 @@ class mywindow(QtWidgets.QWidget, Ui_Form):
         self.helloButton.setToolTip("This is a <b>QPushButon</b> widget")
 
     def OpenFans(self):
-        pass
+        self.Status.setText("状态：打开")
+        os.popen('mosquitto_pub -t gpio -h *** -m "{\"pin\":17,\"value\":1}"')
 
     def CloseFans(self):
-        pass
+        self.Status.setText("状态：关闭")
+        os.popen('mosquitto_pub -t gpio -h *** -m "{\"pin\":17,\"value\":0}"')
 
     def DashBoardPage(self):
         pass
@@ -29,6 +33,14 @@ class mywindow(QtWidgets.QWidget, Ui_Form):
 
     def AdditionPage(self):
         pass
+
+    def closeEvent(self, QCloseEvent):
+        reply = QMessageBox.question(self, "关闭提示", "真的要退出吗？", QMessageBox.Yes |
+                                     QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            QCloseEvent.accept()
+        else:
+            QCloseEvent.ignore()
 
 if __name__==  '__main__':
     app = QtWidgets.QApplication(sys.argv)
